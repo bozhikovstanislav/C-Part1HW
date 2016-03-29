ca0c351bd9a2556c31f3fe3302caa02a070322ca
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-03-29 14:56:19 +0300

    Sequance

--- CSharpPartOne/Age/Program.cs
+++ CSharpPartOne/Age/Program.cs
@@ -9,12 +9,23 @@
             var  birDateof=  Console.ReadLine();
             DateTime nowDateTime=DateTime.Now;
             DateTime birDateTime = DateTime.Parse(String.Format("{0:mm.dd.yyyy}",birDateof));
+            var bmonth = birDateTime.Month;
+            var bdate = birDateTime.Date;
             var byear = birDateTime.Year;
             var nowyear = nowDateTime.Year;
-            var age = nowyear - byear;
+            bool islessthen = bmonth > nowDateTime.Month;
+            var age = 0;
+            if (islessthen)
+            {
+                 age = (nowyear - 1) - byear;
+            }
+            else
+            {
+                age = nowyear - byear;
+            }
+
             Console.WriteLine(age);
             Console.WriteLine(age + 10);
-
         }
     }
 }
---
+++ CSharpPartOne/LongSequance/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LongSequance
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 1001; i++)
+            {
+                var number = -i * Math.Pow(-1, i - 1);
+                Console.WriteLine(number);
+
+            }
+
+        }
+    }
+}
