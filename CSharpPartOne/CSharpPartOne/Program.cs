a9a3a259d5c033cb7c01ca0e6c8fbe423452d1d2
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-03-29 10:09:35 +0300

    Printing Numbers

--- CSharpPartOne/CSharpPartOne/Program.cs
+++ CSharpPartOne/CSharpPartOne/Program.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpPartOne
+﻿namespace CSharpPartOne
 {
+    using System;
+
     class Program
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Hello C#");
 
         }
     }
