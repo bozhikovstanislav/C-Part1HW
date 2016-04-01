c86929e20a676e19ad4ed527516a9be1cd225bfc
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-04-01 23:38:27 +0300

    odd even

--- CSharpPartOne/AsciiChars/Program.cs
+++ CSharpPartOne/AsciiChars/Program.cs
@@ -8,7 +8,10 @@ namespace AsciiChars
     {
         static void Main(string[] args)
         {
-
+            for (int i = 33; i < 127; i++)
+            {
+                Console.Write((char)i);
+            }
         }
     }
 }
---
+++ CSharpPartOne/ODDEVN/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ODDEVN
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberString = Console.ReadLine();
+
+            var isNumber = false;
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                if(47<=(char)numberString[i]&&(char)numberString[i]<=58)
+                {
+                    isNumber = true;
+                }
+            }
+            if (isNumber)
+            {
+                var numberToCheck = int.Parse(numberString);
+
+                if (numberToCheck % 2 == 0)
+                {
+                    Console.WriteLine("even {0}", numberToCheck);
+                }
+                else
+                {
+                    Console.WriteLine("odd {0}", numberToCheck);
+                }
+            }
+        }
+    }
+}
