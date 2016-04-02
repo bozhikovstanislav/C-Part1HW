f1517de4030ef77e35e6874a07094d2b227a28c8
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-04-02 23:03:16 +0300

    rect

---
+++ CSharpPartOne/CircleRectangle/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace CircleRectangle
+{
+    using System;
+
+    class Program
+    {
+        struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+        }
+
+        struct Circle
+        {
+            public Point Point { get; set; }
+            public double Radius { get; set; }
+
+            public double distance(Point point1, Point point2)
+            {
+                var distance =
+                    Math.Sqrt((point2.X - point1.X)*(point2.X - point1.X) + (point2.Y - point1.Y)*(point2.Y - point1.Y));
+                return distance;
+            }
+        }
+
+        struct Rectungle
+        {
+            public Point Pbegining { get; set; }
+            public double WidPoint { get; set;}
+            public double HPoint { get; set; }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Point Xrecatngle=new Point();
+            Xrecatngle.X = 1;
+            Xrecatngle.Y = -1;
+
+            Rectungle rectangle = new Rectungle();
+            rectangle.Pbegining = Xrecatngle;
+            rectangle.WidPoint = 6;
+            rectangle.HPoint = 2;
+
+            Point cercleCenter=new Point();
+
+            cercleCenter.X = 1;
+            cercleCenter.Y = 1;
+
+            Circle circleOne=new Circle();
+            circleOne.Point = cercleCenter;
+            circleOne.Radius = 1.5;
+
+
+            var pointX = double.Parse(Console.ReadLine());
+            var pointY = double.Parse(Console.ReadLine());
+
+            Point pointTocheck=new Point();
+            pointTocheck.X = pointX;
+            pointTocheck.Y = pointY;
+
+            var distanceCircle = circleOne.distance(cercleCenter, pointTocheck);
+
+
+        }
+    }
+}
---
+++ CSharpPartOne/Trapecoid/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Trapecoid
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+
+            if (-500 < a && a < 500 && -555 < b && b < 500 && -555 < h && h < 500)
+            {
+                double v = (a + b) / 2;
+                var area = h* v;
+            Console.WriteLine("{0:f7}", area);
+
+            }
+         
+        }
+    }
+}
