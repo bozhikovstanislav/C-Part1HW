cb025aba63dd807fd769f2126aff3bc2d0a154c2
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-04-02 15:39:13 +0300

    moon

---
+++ CSharpPartOne/MoonWight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace MoonWight
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+            var manWiegh = Console.ReadLine();
+
+           double  number;
+            var manEarthWeigth = double.TryParse(manWiegh, out number);
+            if (0 < number && number < 1000)
+            {
+                if (manEarthWeigth)
+                {
+                    var weightOnMoon = number * 0.17;
+                    Console.WriteLine("{0:f3}", weightOnMoon);
+                }
+               
+            }
+        }
+    }
+}
