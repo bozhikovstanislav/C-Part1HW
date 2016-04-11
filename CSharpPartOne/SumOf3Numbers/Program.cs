dff70f9db405afc9a053b380678d3ef0ae3a8aec
Stanislav bozhikov <bozhikov.stanislav@gmail.com>
2016-04-11 10:39:40 +0300

    3Numbers

---
+++ CSharpPartOne/SumOf3Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SumOf3Numbers
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOne = int.Parse(Console.ReadLine());
+            var numberTwo = int.Parse(Console.ReadLine());
+            var numberTree = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            if ((-1000 <= numberTree && numberTree <= 1000) && (-1000 <= numberTwo && numberTwo <= 1000 )&&
+                (-1000 <= numberOne && numberOne <= 1000))
+            {
+                sum = numberTwo + numberOne + numberTree;
+               
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
